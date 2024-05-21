10d2c95b033e53df3b008ea80ece939e065a02db
mxakmaljanov <maxmudxonakmaljanov@gmail.com>
2024-05-21 23:55:21 +0500

    4-oy uy ishii

---
+++ index.js
@@ -0,0 +1,43 @@
+//1-misol
+//let counter;
+//let son =+prompt("sonni kiriting");
+//let toq;
+//if(son%2==1){
+//toq=son%2==1
+//}
+//console.log(toqson);
+//2-misol
+//let son=+prompt("sonni kiriting");
+//if(son>0){
+//    console.log(musbat son);
+//}
+//else{
+//    console.log(son musbat emas);
+//}
+
+//3-misol
+//let son1=+prompt("1-sonni kiiting");
+//let son2=+prompt("2-sonni kiiting");
+//if(son1>=0 || son2 <-2){
+//    console.log(true);
+//}
+//else{
+//    console.log(false);
+//}
+//4-misol
+//let son1=+prompt("1-sonni kiiting");
+//let son2=+prompt("2-sonni kiiting");
+//if(son1>2|| son2<=3){
+//    console.log ( true)
+//}else{
+//    console.log(false)
+//}
+//5-misol
+//let son=+prompt("sonni kiriting");
+//if(son%2==0){
+//    console.log(juftson);
+//}
+//else{
+//    console.log(toqson) };
+
+
